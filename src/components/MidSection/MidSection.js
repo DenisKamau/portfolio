60deab5fda1aa94eee02a3301353018ef851a8f2
Denis Kimani <denokim55@gmail.com>
2022-05-21 11:04:22 +0300

    Removed flow section

--- src/components/MidSection/MidSection.js
+++ src/components/MidSection/MidSection.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Services from "../Services/Services";
 import Work from "../Work/Work";
-import FlowSection from "../Workflow/FlowSection";
+// import FlowSection from "../Workflow/FlowSection";
 
 const MidSection = () => {
   return (
     <div>
       <Services />
       <Work />
-      <FlowSection />
+      {/* <FlowSection /> */}
     </div>
   );
 };
