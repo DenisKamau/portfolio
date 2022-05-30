99f8e782ca7d1edacd704f25a4cc013f57a507db
Denis <denokim55@gmail.com>
2022-05-30 08:54:42 +0300

    Added services content

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -22,30 +22,22 @@ const Services = () => {
             My awesome <br />
             <span>services</span>
           </h2>
-          <p className="services__Paragraph">
-            Fuelled by a passion for designing and creating compelling products, I have a deep desire to excel and continuously improve in my work.
-          </p>
+          <p className="services__Paragraph">Fuelled by a passion for designing and creating compelling products, I have a deep desire to excel and continuously improve in my work.</p>
           <div className="services__Row">
             <div className="service_Box">
               <img src={icon1} alt="icon1" />
               <h4>Designer</h4>
-              <p className="caption">
-                I design beautiful and powerful websites for modern businesses. Any business today needs a website that wins customers’ trust and
-                helps you do your business well. I make sure your website is up to that standard.
-              </p>
+              <p className="caption">I design beautiful and powerful websites for modern businesses. Any business today needs a website that wins customers’ trust and helps you do your business well. I make sure your website is up to that standard.</p>
             </div>
             <div className="service_Box">
               <img src={icon2} alt="icon2" />
               <h4>Developer</h4>
-              <p className="caption">
-                I build websites in Webflow or using React where I can create responsive, powerful and fully custom websites. Plus, Webflow has an
-                incredibly simple Content Editor for you and your team to edit website content quickly and easily.
-              </p>
+              <p className="caption">I build websites in Webflow or using React where I can create responsive, powerful and fully custom websites. Plus, Webflow has an incredibly simple Content Editor for you and your team to edit website content quickly and easily.</p>
             </div>
             <div className="service_Box lastBox">
               <img src={icon3} alt="icon3" />
               <h4>UI/Ux</h4>
-              <p className="caption">I create User friendly interfaces for your website and a web interface that is aesthetically pleasing.</p>
+              <p className="caption">I create User friendly interfaces for your website and a web interface that is aesthetically pleasing. I translate creative software design concepts and ideas into reality using front end technology such as Vue, React .</p>
             </div>
           </div>
         </div>
